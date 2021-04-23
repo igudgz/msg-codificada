7035020a80a1d7864fa431c530afb35c68cf478a
igudgz <igor220597@hotmail.com>
2021-04-23 15:59:03 -0300

    primeira codificacao

--- dom.js
+++ dom.js
@@ -8,6 +8,9 @@ const codificar = selector(".codificar");
 const decodificar = selector(".decodificar");
 const cifraDeCesar = selector("#apresentacao-cesar");
 const base64 = selector("#apresentacao-base64");
+const mensagem = selector("#mensagem");
+
+// Eventos!
 
 escolhaCodificacao.addEventListener("change", () => {
   if (escolhaCodificacao.value == "cifraDeCesar") {
@@ -31,6 +34,23 @@ inputDecodificar.addEventListener(
   "click",
   () => (decodificar.style.display = "block")
 );
+codificar.addEventListener("click", (event) => {
+  event.preventDefault();
+  let mensagemCodificada = btoa(mensagem.value);
+  if (escolhaCodificacao.value == "base64") {
+    mensagem.value = `${mensagemCodificada}`;
+  }
+});
+
+decodificar.addEventListener("click", (event) => {
+  event.preventDefault();
+  let mensagemDecodificada = atob(mensagem.value);
+  if (escolhaCodificacao.value == "base64") {
+    mensagem.value = `${mensagemDecodificada}`;
+  }
+});
+
+//Funções
 function selector(elemento) {
   return document.querySelector(elemento);
 }
